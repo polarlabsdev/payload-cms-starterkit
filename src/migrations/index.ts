be5e9665f41abb8f83dfca3b87b3b92d91ff2865
Garv <garv1908@gmail.com>
2026-05-28 02:45:09 -0400

    add initial migration using `migrate:create`

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260528_054013 from './20260528_054013';
+
+export const migrations = [
+  {
+    up: migration_20260528_054013.up,
+    down: migration_20260528_054013.down,
+    name: '20260528_054013'
+  },
+];
